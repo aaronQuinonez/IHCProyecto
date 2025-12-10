176a3c9e8a81e4400c29938b524a54af6e8bd9b7
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-12-10 16:34:03 -0500

    Mejorando la visualización del teclado virtual: ajustes en el diseño y etiquetas de notas

--- src/piano/virtual_keyboard.py
+++ src/piano/virtual_keyboard.py
@@ -4,6 +4,7 @@
 Created on Fri Aug 27 22:57:59 2021
 
 @author: mherrera
+Modified for visual improvement
 """
 
 import cv2
@@ -11,12 +12,6 @@ import numpy as np
 import math
 from src.utils import round_half_up
 from src.vision.stereo_config import StereoConfig
-
-# black keys averaging 13.7 mm (0.54 in) and
-# white keys about 23.5 mm (0.93 in) at the
-# base, disregarding space between keys
-# => 13.7 / 23.5 0
-
 
 class VirtualKeyboard():
     __white_map = {
@@ -49,35 +44,18 @@ class VirtualKeyboard():
         self.canvas_h = canvas_h
 
         if self.canvas_w == 640 and self.canvas_h == 480:
-            # If camera are on the front of the user, the left keyboard
-            # image (on the right of the screen) must be centered at left
-            # Usar configuración centralizada
             self.kb_x0 = int(round_half_up(canvas_w * StereoConfig.KEYBOARD_X0_RATIO))
             self.kb_y0 = int(round_half_up(canvas_h * StereoConfig.KEYBOARD_Y0_RATIO))
             self.kb_x1 = int(round_half_up(canvas_w * StereoConfig.KEYBOARD_X1_RATIO))
             self.kb_y1 = int(round_half_up(canvas_h * StereoConfig.KEYBOARD_Y1_RATIO))
 
-        # print('Piano Coords: (x0,y0) (x1,y1): ({},{}) ({}, {})'.format(
-        #     self.kb_x0, self.kb_y0, self.kb_x1, self.kb_y1))
-
         self.kb_white_n_keys = kb_white_n_keys
-
         self.kb_len = self.kb_x1 - self.kb_x0
-        print('virtual_keyboard:kb_len:{}'.format(self.kb_len))
         self.white_kb_height = self.kb_y1 - self.kb_y0
-        print('virtual_keyboard:kb_height:{}'.format(self.white_kb_height))
-
         self.white_key_width = self.kb_len/kb_white_n_keys
-        print('virtual_keyboard:key_width:{}'.format(self.white_key_width))
-
-        # Usar relación de tamaño desde configuración centralizada
+        
         self.black_key_width = self.white_key_width * (StereoConfig.BLACK_KEY_WIDTH_RATIO / StereoConfig.WHITE_KEY_WIDTH_RATIO)
-
-        print('virtual_keyboard:black_key_width:{}'.
-              format(self.black_key_width))
         self.black_key_heigth = self.white_kb_height * StereoConfig.BLACK_KEY_HEIGHT_RATIO
-        print('virtual_keyboard:black_key_heigth:{}'.
-              format(self.black_key_heigth))
 
         self.keys_without_black = \
             list({none_keys for none_keys in self.__black_map
@@ -86,17 +64,17 @@ class VirtualKeyboard():
         self.key_id = None
         self.rectangle = []
         self.upper_zone_divisions = []
+        
+        # Lista de nombres de notas para visualización
+        self.white_key_names = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"]
 
     def new_key(self, key_id, top_left, bottom_rigth):
         self.key_id = key_id
         self.rectangle = [top_left, bottom_rigth]
         return key_id, self.rectangle
 
-    # def add_key_key_upper_zone(self):
-
     def draw_virtual_keyboard(self, img):
-        # Prepara shapes
-        # Initialize blank mask image of same dimensions for drawing the shapes
+        # Prepara shapes (Fondo blanco semitransparente para las teclas)
         shapes = np.zeros_like(img, np.uint8)
         cv2.rectangle(
             img=shapes,
@@ -105,87 +83,85 @@ class VirtualKeyboard():
             color=(255, 255, 255),
             thickness=cv2.FILLED)
 
-        # Generate output by blending image with shapes image, using the shapes
-        # images also as mask to limit the blending to those parts
-        # Usar alpha desde configuración centralizada
         alpha = StereoConfig.KEYBOARD_ALPHA
         mask = shapes.astype(bool)
         img[mask] = cv2.addWeighted(img, alpha, shapes, 1 - alpha, 0)[mask]
 
+        # Configuración de fuente para las etiquetas
+        font_face = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.55
+        font_thickness = 2
+        text_color = (0, 0, 0) # Negro elegante
+
         for p in range(self.kb_white_n_keys):
             x_line_pos = self.kb_x0 + self.white_key_width * (p+1)
 
-            # Draw black keys
-            # Para 8 teclas blancas (Do a Do siguiente octava):
-            # Las teclas negras están entre: 0-1 (Do#), 1-2 (Re#), 3-4 (Fa#), 4-5 (Sol#), 5-6 (La#)
-            # No hay tecla negra entre E-F (posición 2-3) ni B-C (posición 6-7)
+            # --- DIBUJAR TECLAS NEGRAS ---
             if p not in self.keys_without_black:
-                # Teclas negras a la izquierda: Do#, Fa#, Sol#
-                if p in (0, 3, 4):
-                    b_bk_x0 = int(round_half_up(
-                        x_line_pos - self.black_key_width*(2/3)))
-                    b_bk_x1 = int(round_half_up(
-                        x_line_pos + self.black_key_width*(1/3)))
-                # Teclas negras a la derecha: Re#, La#
-                elif p in (1, 5):
-                    b_bk_x0 = int(round_half_up(
-                        x_line_pos - self.black_key_width*(1/3)))
-                    b_bk_x1 = int(round_half_up(
-                        x_line_pos + self.black_key_width*(2/3)))
-                else:
-                    b_bk_x0 = int(round_half_up(
-                        x_line_pos - self.black_key_width/2))
-                    b_bk_x1 = int(round_half_up(
-                        x_line_pos + self.black_key_width/2))
+                if p in (0, 3, 4): # Izquierda
+                    b_bk_x0 = int(round_half_up(x_line_pos - self.black_key_width*(2/3)))
+                    b_bk_x1 = int(round_half_up(x_line_pos + self.black_key_width*(1/3)))
+                elif p in (1, 5): # Derecha
+                    b_bk_x0 = int(round_half_up(x_line_pos - self.black_key_width*(1/3)))
+                    b_bk_x1 = int(round_half_up(x_line_pos + self.black_key_width*(2/3)))
+                else: # Centro
+                    b_bk_x0 = int(round_half_up(x_line_pos - self.black_key_width/2))
+                    b_bk_x1 = int(round_half_up(x_line_pos + self.black_key_width/2))
 
+                # Tecla negra rellena
                 cv2.rectangle(
                     img=img,
                     pt1=(b_bk_x0, self.kb_y0),
-                    pt2=(b_bk_x1, int(
-                        round_half_up(self.kb_y0 + self.black_key_heigth))),
+                    pt2=(b_bk_x1, int(round_half_up(self.kb_y0 + self.black_key_heigth))),
                     color=(0, 0, 0),
                     thickness=cv2.FILLED)
+                
+                # Borde gris sutil para las teclas negras
+                cv2.rectangle(
+                    img=img,
+                    pt1=(b_bk_x0, self.kb_y0),
+                    pt2=(b_bk_x1, int(round_half_up(self.kb_y0 + self.black_key_heigth))),
+                    color=(50, 50, 50),
+                    thickness=1)
 
-                key_coord = \
-                    self.new_key(p,
-                                 (b_bk_x0, self.kb_y0),
-                                 (b_bk_x1,
-                                  int(
-                                      round_half_up(self.kb_y0 +
-                                                    self.black_key_heigth))))
-                self.upper_zone_divisions.append(key_coord)
+                self.new_key(p, (b_bk_x0, self.kb_y0),
+                             (b_bk_x1, int(round_half_up(self.kb_y0 + self.black_key_heigth))))
+                self.upper_zone_divisions.append(self.rectangle)
 
+            # --- LÍNEAS SEPARADORAS DE TECLAS BLANCAS ---
             cv2.line(img=img,
                      pt1=(int(round_half_up(x_line_pos)), self.kb_y0),
                      pt2=(int(round_half_up(x_line_pos)), self.kb_y1),
-                     color=(0, 0, 0),
-                     thickness=2)
+                     color=(0, 0, 0), # Línea negra delgada
+                     thickness=1)
 
-            if p != 7:  # Do central
-                c_color = (0, 255, 0)
-            else:
-                c_color = (0, 0, 0)
+            # --- ETIQUETAS DE NOTAS (Nuevo diseño limpio) ---
+            
+            # 1. Obtener nombre de la nota
+            note_name = self.white_key_names[p % 7]
+            
+            # 2. Calcular el tamaño exacto del texto para centrarlo
+            (text_w, text_h), baseline = cv2.getTextSize(note_name, font_face, font_scale, font_thickness)
+            
+            # 3. Calcular posición central X de la tecla
+            key_center_x = x_line_pos - self.white_key_width / 2
+            
+            # 4. Definir posición final del texto (Centrado en X, cerca del fondo en Y)
+            text_x = int(key_center_x - text_w / 2)
+            text_y = int(self.kb_y1 - 15) # 15 píxeles desde el borde inferior
 
-            cv2.circle(img=img,
-                       center=(int(x_line_pos - self.white_key_width/2),
-                               int(self.kb_y0 + self.white_kb_height*3/4)),
-                       radius=7,
-                       color=c_color,
-                       thickness=cv2.FILLED
-                       )
+            # 5. Dibujar texto limpio
+            cv2.putText(img=img, text=note_name,
+                        org=(text_x, text_y),
+                        fontFace=font_face,
+                        fontScale=font_scale,
+                        color=text_color,
+                        thickness=font_thickness,
+                        lineType=cv2.LINE_AA) # LINE_AA para bordes suaves
 
-            cv2.putText(img=img, text=str(p+1),
-                        org=(int(round_half_up(
-                            x_line_pos-self.white_key_width/2))-7,
-                            int(round_half_up(
-                                self.kb_y0 + self.white_kb_height*3/4))+3),
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                        fontScale=0.4,
-                        color=(0, 0, 255))
-
+        # Borde exterior del teclado
         cv2.rectangle(img, (self.kb_x0, self.kb_y0),
-                      (self.kb_x1, self.kb_y1), (255, 0, 0), 2)
-
+                      (self.kb_x1, self.kb_y1), (0, 0, 0), 2)
 
     def intersect(self, pointXY):
         if pointXY[0] > self.kb_x0 and pointXY[0] < self.kb_x1 and \
@@ -193,35 +169,15 @@ class VirtualKeyboard():
             return True
         return False
 
-    # def find_key(self, x_pos):
-    #     print('find_key:x_pos {}'.format(x_pos))
-    #     key = (x_pos/self.white_key_width)
-    #     print('find_key:key {}'.format(key))
-    #     key = math.floor(key)
-    #     print('find_key:ceil key {}'.format(key))
-    #     return int(key-1)
-
     def find_key_in_upper_zone(self, x_kb_pos, y_kb_pos):
-        # print('find_key_in_upper_zone:x_pos:{}'.format(x_kb_pos))
-
         key_id = -1
         for k in self.upper_zone_divisions:
             if x_kb_pos > k[1][0][0] and x_kb_pos < k[1][1][0]:
-
-                # print('k:{}'.format(k))
-                # print('k[0]:{}'.format(k[0]))
-                # print('k[1][0][0]:{}'.format(k[1][0][0]))
-                # print('k[1][1][0]:{}'.format(k[1][1][0]))
-
                 key_id = k[0]
-                # print('Key_id:{}'.format(key_id))
                 break
         return key_id
 
     def find_key(self, x_pos, y_pos):
-        # print('find_key:x_pos {}'.format(x_pos))
-        # print('find_key:y_pos {}'.format(y_pos))
-
         x = x_pos - self.kb_x0
         y = y_pos - self.kb_y0
 
@@ -230,7 +186,6 @@ class VirtualKeyboard():
             key = math.floor(key)
 
             key = self.find_key_in_upper_zone(x_pos, y_pos)
-            # print('find_key:upper zone key {}'.format(key))
             if key == -1:
                 key = x/self.white_key_width
                 key = math.floor(key)
